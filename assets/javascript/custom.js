4e6fdd6e60d5df0390c1489f8cc4639da4fef9c7
aditya <aditya.jain@preciousinfosystem.com>
2024-08-06 19:03:12 +0530

    more page design

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -6,13 +6,13 @@ $(document).ready(function () {
         nav: true,
         responsive: {
             0: {
-                items: 1,
-            },
-            600: {
                 items: 3,
             },
-            1000: {
+            600: {
                 items: 5,
+            },
+            1000: {
+                items: 7,
             },
         },
     });
